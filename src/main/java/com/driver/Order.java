336802a5eba0fd4ff0e80a838e1a942857896eea
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2024-05-23 23:54:36 +0530

    Update Order.java

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,6 +1,56 @@
+// package com.driver;
+
+// <<<<<<< HEAD
+
+
+
+// public class Order {
+
+//     public void setId(String id) {
+//         this.id = id;
+//     }
+
+//     public void setDeliveryTime(int deliveryTime) {
+//         this.deliveryTime = deliveryTime;
+//     }
+
+//     private String id;
+//     private int deliveryTime;
+    
+//    // @PostMapping("/orders/add-order")
+//     public Order(String id, String deliveryTime) {
+
+//         // The deliveryTime has to converted from string to int and then stored in the attribute
+//         //deliveryTime  = HH*60 + MM
+//         this.id=id;
+//         //int temp=Integer.parseInt(deliveryTime);
+//          int hours = Integer.valueOf(deliveryTime.substring(0,2));
+//         int minutes = Integer.valueOf(deliveryTime.substring(3));
+//         int total = hours*60 + minutes;
+        
+//         this.deliveryTime=total;
+// =======
+// public class Order {
+
+//     private String id;
+//     private int deliveryTime;
+
+//     public Order(String id, String deliveryTime) {
+
+//         // The deliveryTime has to converted from string to int and then stored in the attribute
+//         //deliveryTime  = HH*60 + MM
+// >>>>>>> origin/master
+//     }
+
+//     public String getId() {
+//         return id;
+//     }
+
+//     public int getDeliveryTime() {return deliveryTime;}
+// }
 package com.driver;
 
-<<<<<<< HEAD
+
 
 
 
@@ -15,6 +65,14 @@ public class Order {
     }
 
     private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
     private int deliveryTime;
     
    // @PostMapping("/orders/add-order")
@@ -29,22 +87,7 @@ public class Order {
         int total = hours*60 + minutes;
         
         this.deliveryTime=total;
-=======
-public class Order {
 
-    private String id;
-    private int deliveryTime;
 
-    public Order(String id, String deliveryTime) {
-
-        // The deliveryTime has to converted from string to int and then stored in the attribute
-        //deliveryTime  = HH*60 + MM
->>>>>>> origin/master
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public int getDeliveryTime() {return deliveryTime;}
+}
 }
